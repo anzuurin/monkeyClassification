8c414d691169080043c1d8f010428459b7cd44a8
Melody Llinas <mellinas@email.wm.edu>
2023-11-30 12:20:37 -0500

    file walk traversal

--- color2grayscale.py
+++ color2grayscale.py
@@ -2,6 +2,18 @@
 Project by Melody Llinas & Melody Vu
 
 The following script will take our in-color image dataset and convert them all to grayscale
-automatically.
+automatically. The images were taken from a monkey species dataset from kaggle 
+(via https://www.google.com/url?q=https://www.kaggle.com/datasets/slothkong/10-monkey-species/&sa=D&source=docs&ust=1701357394325106&usg=AOvVaw3E7ABWaJSivMcEKtBl3ldz)
 
 '''
+import os
+
+def nameFirstImage():
+    directory = "/Users/Melody/Desktop/Projects/ML/imageRecolorization/images"
+    for root, dirs, files, in os.walk(directory):
+        print("root:", root)
+        print("sub-folder:", dirs)
+        print("inside contents:", files)
+        print('----------------------------------')
+
+nameFirstImage()
