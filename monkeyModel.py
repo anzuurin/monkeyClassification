48d11f623830b2521cc602c4701cf903b82c2314
Melody Vu <mvu@email.wm.edu>
2023-12-02 20:03:49 -0500

    first draft cnn and test file

--- monkeyModel.py
+++ monkeyModel.py
@@ -1,9 +1,111 @@
-'''
-The following model is what we will train and use to classify images
-of monkeys.
-'''
+import numpy as np
+import time
+import matplotlib.pyplot as plt
 
 import tensorflow as tf
-from tensorflow import keras
-from keras.models import Model
-from keras.layers import Layer
+from keras.models import load_model
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+
+# parameters
+IMG = 200
+IMG_SIZE = [IMG, IMG]
+numOfClasses = 10
+batchSize = 32
+EPOCHS = 30
+
+# building the model
+model = tf.keras.models.Sequential ([
+
+    #convolution layers
+    tf.keras.layers.Conv2D(32, (3,3), activation='relu', input_shape=(IMG,IMG,3)),
+    tf.keras.layers.MaxPooling2D(2,2),
+
+    tf.keras.layers.Conv2D(32, (3,3), activation='relu', input_shape=(IMG,IMG,3)),
+    tf.keras.layers.MaxPooling2D(2,2),
+
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu', input_shape=(IMG,IMG,3)),
+    tf.keras.layers.MaxPooling2D(2,2),
+
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu', input_shape=(IMG,IMG,3)),
+    tf.keras.layers.MaxPooling2D(2,2),
+
+    # flattening
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dropout(0.5), #prevent overfitting, randomly 50% of input units
+
+    # fully connected layers
+    tf.keras.layers.Dense(512, activation='relu'),
+    tf.keras.layers.Dense(numOfClasses, activation='softmax')
+])
+
+print(model.summary())
+
+# compile the model
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# get the data
+trainingFolder = "C:/Users/huydu/introtoml/monkey_class_proj/monkeyClassification/images/training"
+validationFolder = "C:/Users/huydu/introtoml/monkey_class_proj/monkeyClassification/images/validation"
+
+trainingGenerator = ImageDataGenerator( rescale = 1. / 255, #augment/enrich images to get more data
+                                         rotation_range = 20,
+                                         width_shift_range = 0.2,
+                                         height_shift_range = 0.2,
+                                         shear_range = 0.2,
+                                         zoom_range = 0.2,
+                                         horizontal_flip = True)
+
+training_set = trainingGenerator.flow_from_directory( trainingFolder,
+                                                      shuffle = True,
+                                                      target_size = IMG_SIZE,
+                                                      batch_size = batchSize,
+                                                      class_mode = 'categorical')
+
+validationGenerator = ImageDataGenerator( rescale = 1. / 255)
+
+validation_set = validationGenerator.flow_from_directory( validationFolder,
+                                                      shuffle = False, ####################
+                                                      target_size = IMG_SIZE,
+                                                      batch_size = batchSize,
+                                                      class_mode = 'categorical')
+
+# get step number (rounding up)
+trainingSteps = np.ceil(training_set.samples / batchSize) 
+validationSteps = np.ceil(validation_set.samples / batchSize) 
+
+# store best model found during training
+bestModel_file = "C:/Users/huydu/introtoml/monkey_class_proj/monkeyClassification/bestMonkeyModel.h5"
+bestModel = ModelCheckpoint(bestModel_file, monitor='val_accuracy', verbose=1, save_best_only=True)
+
+# train the model
+history = model.fit( training_set,
+                    validation_data = validation_set,
+                    epochs = EPOCHS,
+                    steps_per_epoch = trainingSteps,
+                    validation_steps = validationSteps,
+                    verbose = 1,
+                    callbacks = [bestModel])
+
+# evaluate the model
+valResults = model.evaluate(validation_set)
+print(valResults)
+print(model.metrics_names)
+
+# chart the results
+trainAcc = history.history['accuracy']
+validationAcc = history.history['val_accuracy']
+trainLoss = history.history['loss']
+validationLoss = history.history['val_loss']
+
+actualEpochs = range(len(trainAcc))
+print("Actual Epochs: "+ str(actualEpochs))
+
+plt.plot(actualEpochs, trainAcc, 'r', label="Training Accuracy")
+plt.plot(actualEpochs, validationAcc, 'b', label="Validation Accuracy")
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.title('Training and Validation Accuracy vs Epochs')
+
+plt.show()
--- monkeyModelTest.py
+++ monkeyModelTest.py
@@ -1,5 +1,4 @@
 import numpy as np
-import time
 import matplotlib.pyplot as plt
 import pandas as pd
 import glob
@@ -9,31 +8,32 @@ import tensorflow as tf
 from keras.models import load_model
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+#from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 
-#parameters
+# parameters
 imageSize = [200, 200]
 batchSize = 32 
 
-#get the best model from training
-bestModel = "" #path to model file
-model = load_model(bestModel)
+# get the best model from training
+bestModel_file = "C:/Users/huydu/introtoml/monkey_class_proj/monkeyClassification/bestMonkeyModel.h5" #path to model file
+model = load_model(bestModel_file)
 
-#get validation data
+# get validation data
 validationFolder = "" #path to validation folder
 validation_generator = ImageDataGenerator(rescale = 1. /255)
 validation_set = validation_generator.flow_from_directory(validationFolder, shuffle = False, target_size = imageSize, 
                                                           batch_size = batchSize, class_mode = 'categorical')
 
-#predict the species of the validation set using the model
+# predict the species of the validation set using the model
 predictions = model.predict(validation_set)
 predictionResults = np.argmax(predictions, axis=1)
 print(predictionResults)
 
-#print out monkey_labels.txt
+# print out monkey_labels.txt
 columns = ["Label", "Latin Name", "Common Name", "Train Images", "Validation Images"]
-df = pd.read_csv("======PUT PATH TO monkey_labels.txt", names=columns, skiprows=1)
-#trim stuff for formatting
+# df is a Data Frame containing the info from monkey_labels.txt
+df = pd.read_csv("C:/Users/huydu/introtoml/monkey_class_proj/monkeyClassification/images/monkey_labels.txt", names=columns, skiprows=1) 
+# trim stuff for formatting
 df['Label'] = df['Label'].str.strip()
 df['Latin Name'] = df['Latin Name'].replace("\t", "")
 df['Latin Name'] = df['Latin Name'].str.strip()
@@ -41,7 +41,7 @@ df['Common Name'] = df['Common Name'].str.strip()
 df = df.set_index("Label")
 print(df)
 
-#print out random 30 pics with the predicted species next to real species
+# print out random 30 pics with the predicted species next to real species
 monkeyNames = df["Common Name"]
 def compareResults():
     image_files = glob.glob(validationFolder + '/*/*.jpg')
@@ -53,20 +53,20 @@ def compareResults():
     correct = 0
 
     for i in range(numPics):
-        #pick random picture
+        # pick random picture
         x = random.choice(image_files)
         xInd = image_files.index(x)
         xImage = plt.imread(x)
 
-        #get prediction for this pic
+        # get prediction for this pic
         xPred = monkeyNames[predictionResults[xInd]]
         xPred = xPred[:7]
 
-        #get real species for this pic
+        # get real species for this pic
         xReal = monkeyNames[validation_set.classes[xInd]]
         xReal = xReal[:7]
 
-        #if correct, record
+        # if correct, record
         if(xPred == xReal):
             correct += 1
         
@@ -79,5 +79,5 @@ def compareResults():
 
     plt.show()
 
-#run the function
+# run the function
 compareResults()
